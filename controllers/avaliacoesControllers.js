5d6fcb6d90bfc7c620d5f90d376995ca16c0b03a
ricardo8santana <luisriccardo@proton.me>
2024-08-02 10:01:44 -0300

    correcao

---
+++ controllers/avaliacoesControllers.js
@@ -0,0 +1,21 @@
+const dbConecta = require('../models/dbConnection');
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    const query = `select 
+    a.*,
+    u.username  as nome_usuarios
+    from avaliacoes a 
+    inner join usuarios u on
+    u.id  = a.usuario_id `;
+    dbConecta.query(query, (err, results) => {
+        if(err) throw err;
+        res.json(results);
+    });  
+});
+
+
+
+
+module.exports = router
---
+++ controllers/usuariosControllers.js
@@ -0,0 +1,22 @@
+const dbConecta = require('../models/dbConnection');
+const express = require('express');
+const router = express.Router();
+
+router.get("/usuarios", (req, res) => {
+    const query = 'select * from  usuarios';
+    dbConecta.query(query, (err, results) => {
+        if(err) throw err;
+        res.json(results);
+    });  
+});
+
+router.get("/:username", (req, res) => {
+    const  username = req.params.username;
+    const query = 'select * from filmes where username = ?';
+    dbConecta.query(query,[username], (err, results) => {
+        if(err) throw err;
+        res.json(results);
+    });
+});
+
+module.exports = router
